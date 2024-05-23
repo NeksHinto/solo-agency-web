f505ab6225e7f3b06ec4901f6a577a3ff23a0b89
NeksHinto <nhinojo@itba.edu.ar>
2024-05-23 08:54:02 -0400

    Update servicesCarousel.js

--- src/app/components/servicesCarousel.js
+++ src/app/components/servicesCarousel.js
@@ -66,10 +66,9 @@ export default function ServicesCarousel() {
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(12, 1fr)",
-          gridTemplateRows: "minmax(auto, 1fr) minmax(auto, 1fr)",
+          gridTemplateRows: "minmax(auto, 1fr) minmax(auto, 1fr) minmax(auto, 1fr) minmax(auto, 1fr)",
           gridTemplateAreas: `
-          'box1 box1 box1 box1 box2 box2 box2 box2 box3 box3 box3 box3' 
-          'box4 box4 box4 box5 box5 box5 box6 box6 box6 box7 box7 box7'
+          'box1 box1 box2 box2' 'box3 box3 box3 box3' 'box4 box4 box5 box5' 'box6 box6 box7 box7'
           `,
         }}
       >
@@ -87,7 +86,7 @@ export default function ServicesCarousel() {
                 gridArea: `box${i + 1}`,
               }}
             >
-              <Link href="/services">{service.name}</Link>
+              <Link href="/clients">{service.name}</Link>
             </p>
           );
         })}
