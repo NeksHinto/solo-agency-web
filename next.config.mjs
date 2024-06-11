463b6f0a58019e642f24ff71d21613fd463be5d8
NeksHinto <nhinojo@itba.edu.ar>
2024-06-11 17:15:00 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   images: {
-  //     dangerouslyAllowSVG: true,
-  //     loader: "custom",
-  //     loaderFile: "./src/app/imageLoader.js",
-  //     remotePatterns: [
-  //       {
-  //         protocol: "https",
-  //         hostname: "soloagency.netlify.app",
-  //         port: "",
-  //         pathname: "/.netlify/images?url=**",
-  //       },
-  //     ],
-  //   },
+    images: {
+      dangerouslyAllowSVG: true,
+      loader: "custom",
+      loaderFile: "./src/app/imageLoader.js",
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "soloagency.netlify.app",
+          port: "",
+          pathname: "/.netlify/images?url=**",
+        },
+      ],
+    },
 };
 
 export default nextConfig;
