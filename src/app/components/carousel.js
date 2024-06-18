0391b50375d589b716088269b592068ee62eb106
NeksHinto <nhinojo@itba.edu.ar>
2024-06-18 11:47:38 -0400

    fix ios 6

--- src/app/components/carousel.js
+++ src/app/components/carousel.js
@@ -18,7 +18,30 @@ const ClientCarousel = ({ items, carouselItemProps, hqVideos }) => {
     infinite: true,
     // centerMode: true,
     slidesToScroll: 1,
-    variableWidth: true,
+    // variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+    ],
     autoplay: true,
     speed: 3000,
     autoplaySpeed: 3000,
